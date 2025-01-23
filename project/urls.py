5b92c4b17794c7a22db4e4c1cf7a0b49b38cbdc3
Wesley Ordoñez <wesordonez1@gmail.com>
2025-01-23 12:43:02 -0600

    initial commit, added html file, urls and views to main configs

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,7 @@ from django.urls import path
 from django.urls import path, include, re_path
 
 from .views import (rate_limiter_view, view_404, 
-                        handler_403, home_view, landing_view) #subscribe_view
+                        handler_403, home_view, landing_view, about_view) #subscribe_view
 
 from .sitemaps import StaticSitemap
 from blog.sitemaps import BlogSitemap
@@ -47,6 +47,7 @@ urlpatterns = [
     path('landing/', landing_view, name='landing'),
 
     path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
 
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- project/views.py
+++ project/views.py
@@ -31,3 +31,6 @@ def home_view(request):
 
 def landing_view(request):
     return render(request, 'landing.html', status=200)
+
+def about_view(request):
+    return render(request, 'about.html', status=200)
