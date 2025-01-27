8463b52a1cbede32d269750122f60a346a12dd1c
Wesley Ordoñez <wesordonez1@gmail.com>
2025-01-27 14:47:54 -0600

    init commit, added base structure for services page

--- project/views.py
+++ project/views.py
@@ -34,3 +34,6 @@ def landing_view(request):
 
 def about_view(request):
     return render(request, 'about.html', status=200)
+
+def services_view(request):
+    return render(request, 'services.html', status=200)
